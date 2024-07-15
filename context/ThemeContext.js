c5b7899ceb9a44ff6c22f79a1fcde968771412bb
Adnan <hpadnan91@gmail.com>
2024-07-15 17:42:48 +0200

    css modules i theme context izmjene

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -5,17 +5,31 @@ export const ThemeContext = createContext();
 export function ThemeProvider({ children }) {
   const [theme, setTheme] = useState('light');
 
-  const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
-  };
-  
   useEffect(() => {
-   document.body.className=theme
+    // Postavljanje početne teme na osnovu preferencija sistema
+    const systemPreference = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    setTheme(systemPreference);
+
+    // Promjena teme na osnovu promjene preferencija sistema
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const handleChange = () => setTheme(mediaQuery.matches ? 'dark' : 'light');
+
+    mediaQuery.addEventListener('change', handleChange);
+    return () => mediaQuery.removeEventListener('change', handleChange);
+  }, []);
+
+  useEffect(() => {
+    document.body.classList.remove('light', 'dark');
+    document.body.classList.add(theme);
   }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-     {children}
+      {children}
     </ThemeContext.Provider>
   );
 }
